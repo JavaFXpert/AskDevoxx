23e7e9d942a5ef3abf1370d47c889929fcdbfcb9
JavaFXpert <james@j1w.xyz>
2016-08-25 14:51:55 -0400

    Initial commit

---
+++ src/main/java/com/devoxx/watson/AskDevoxxApplication.java
@@ -0,0 +1,12 @@
+package com.devoxx.watson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskDevoxxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskDevoxxApplication.class, args);
+	}
+}
---
+++ src/test/java/com/devoxx/watson/AskDevoxxApplicationTests.java
@@ -0,0 +1,16 @@
+package com.devoxx.watson;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AskDevoxxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
